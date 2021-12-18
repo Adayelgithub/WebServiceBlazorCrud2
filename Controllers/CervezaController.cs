788d48c5635065e6908b9677c495226a508dc5e0
Aday <Aday@DESKTOP-UJ4RDH3>
2021-12-18 16:44:34 +0000

    Agregar archivos de proyecto.

---
+++ Controllers/CervezaController.cs
@@ -0,0 +1,163 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebServiceBlazorCrud.ModelsEntityFramework.Response;
+using WebServiceBlazorCrud.ModelsEntityFramework;
+using WebServiceBlazorCrud.ModelsEntityFramework.Request;
+namespace WebServiceBlazorCrud.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CervezaController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            Respuesta<List<Cerveza>> oRespuesta = new Respuesta<List<Cerveza>>();
+
+            try
+            {
+
+
+                using (blazorcrudContext db = new blazorcrudContext())
+                {
+
+                    var lst = db.Cerveza.ToList();
+                    oRespuesta.Exito = 1;
+                    oRespuesta.Data = lst;
+
+                }
+
+            }
+            catch (Exception ex) {
+
+                oRespuesta.Mensaje = ex.Message;
+
+            }
+                return Ok(oRespuesta);
+        }
+
+        [HttpGet("{Id}")]
+        public IActionResult Get(int Id)
+        {
+            Respuesta<Cerveza> oRespuesta = new Respuesta<Cerveza>();
+
+            try
+            {
+
+
+                using (blazorcrudContext db = new blazorcrudContext())
+                {
+
+                    var lst = db.Cerveza.Find(Id);
+                    oRespuesta.Exito = 1;
+                    oRespuesta.Data = lst;
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                oRespuesta.Mensaje = ex.Message;
+
+            }
+            return Ok(oRespuesta);
+        }
+
+        [HttpPost]
+        public IActionResult Add(CervezaRequest model)
+        {
+            Respuesta<object> oRespuesta = new Respuesta<object>();
+
+            try
+            {
+
+
+                using (blazorcrudContext db = new blazorcrudContext())
+                {
+
+                    Cerveza oCerveza = new Cerveza(); // clase cerveza dentro de models, creado con el comando de scaffold
+                    oCerveza.Marca = model.Marca;
+                    oCerveza.Nombre = model.Nombre;
+                    db.Cerveza.Add(oCerveza);
+                    db.SaveChanges();
+                    oRespuesta.Exito = 1;
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                oRespuesta.Mensaje = ex.Message;
+
+            }
+            return Ok(oRespuesta);
+        }
+
+        [HttpPut] //editar
+        public IActionResult Edit(CervezaRequest model)
+        {
+            Respuesta<object> oRespuesta = new Respuesta<object>();
+
+            try
+            {
+
+
+                using (blazorcrudContext db = new blazorcrudContext())
+                {
+
+                    Cerveza oCerveza = db.Cerveza.Find(model.Id);
+                    oCerveza.Marca = model.Marca;
+                    oCerveza.Nombre = model.Nombre;
+                    db.Entry(oCerveza).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                    db.SaveChanges();
+                    oRespuesta.Exito = 1;
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                oRespuesta.Mensaje = ex.Message;
+
+            }
+            return Ok(oRespuesta);
+        }
+
+
+        [HttpDelete("{Id}")] //eliminar
+        public IActionResult Delete(int Id)
+        {
+            Respuesta<object> oRespuesta = new Respuesta<object>();
+
+            try
+            {
+
+
+                using (blazorcrudContext db = new blazorcrudContext())
+                {
+
+                    Cerveza oCerveza = db.Cerveza.Find(Id);
+                    db.Remove(oCerveza);
+                    db.SaveChanges();
+                    oRespuesta.Exito = 1;
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                oRespuesta.Mensaje = ex.Message;
+
+            }
+            return Ok(oRespuesta);
+        }
+    }
+}
---
+++ ModelsEntityFramework/blazorcrudContext.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace WebServiceBlazorCrud.ModelsEntityFramework
+{
+    public partial class blazorcrudContext : DbContext
+    {
+        public blazorcrudContext()
+        {
+        }
+
+        public blazorcrudContext(DbContextOptions<blazorcrudContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Cerveza> Cerveza { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseMySQL("server=localhost;port=3306;user=root;password=root;database=blazorcrud");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cerveza>(entity =>
+            {
+                entity.ToTable("cerveza");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Marca)
+                    .HasColumnName("marca")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Nombre)
+                    .HasColumnName("nombre")
+                    .HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
